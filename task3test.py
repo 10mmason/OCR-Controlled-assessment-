9962ff4e825957e3b198b094fd5cc50734dcd342
OpenShift guest <10mmason@ashmanorschool.com>
2014-06-11 05:24:56 -0400

    no work

---
+++ task3test.py
@@ -0,0 +1,10 @@
+import task3
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+
+def test1(self):
+    assert task3.booknumber 
+
+if __name__ == '__main__':
+    unittest.main()
